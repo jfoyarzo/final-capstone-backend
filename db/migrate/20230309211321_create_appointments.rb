fc51d55c139774a82c9f863c3f87fcb92bfc60ff
MattGomb <gombosonline@gmail.com>
2023-03-09 16:24:14 -0500

    Create migrations

--- db/migrate/20230309211321_create_appointments.rb
+++ db/migrate/20230309211321_create_appointments.rb
@@ -1,8 +1,8 @@
 class CreateAppointments < ActiveRecord::Migration[7.0]
   def change
     create_table :appointments do |t|
-      t.references :user_id, null: false, foreign_key: { to_table: :users }
-      t.references :investigator_id, null: false, foreign_key: { to_table: :investigators }
+      t.references :user, null: false, foreign_key: true
+      t.references :investigator, null: false, foreign_key: true
       t.datetime :date
       t.string :city
 
