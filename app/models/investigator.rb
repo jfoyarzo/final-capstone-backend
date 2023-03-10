e0e19cb391b443e48633f0c3f95349e6213f9c8c
MattGomb <gombosonline@gmail.com>
2023-03-10 12:07:40 -0500

    Add validations

--- app/models/investigator.rb
+++ app/models/investigator.rb
@@ -1,4 +1,10 @@
 class Investigator < ApplicationRecord
   has_many :appointments
   has_many :users, through: :appointments
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :photo, presence: true
+  validates :description, presence: true, length: { minimum: 3, maximum: 500 }
+  validates :fee, presence: true, numericality: { greater_than: 0 }
+  validates :rating, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 5 }
 end
