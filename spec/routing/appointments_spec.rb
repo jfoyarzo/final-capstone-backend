622eb36c6f888aa3ee0c5e06d0e159eaa48ab620
MattGomb <gombosonline@gmail.com>
2023-03-14 16:23:30 -0500

    Update routing tests

--- spec/routing/appointments_spec.rb
+++ spec/routing/appointments_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'routes for appointments', type: :routing do
+  before(:each) do
+    @user = User.create!(id: 2, name: 'John Doe', email: 'john@doe.com', password: 'Password1$$', admin: true)
+    @investigator = Investigator.create!(id: 1, name: 'Investi Doe', photo: 'https://photos.com/photo1.jpg', description: 'Once a doe, always a doetective', fee: 45.99, rating: 4)
+    @appointment = Appointment.create!(id: 1, user_id: 2, investigator_id: 1, date: '2024-01-01 12:00', city: 'New York, NY')
+  end
+
   it 'routes /appointments to the appointments controller' do
     expect(get: '/v1/appointments').to route_to(
       controller: 'v1/appointments',
@@ -16,18 +22,18 @@ RSpec.describe 'routes for appointments', type: :routing do
   end
 
   it 'routes /appointments/:id to the appointments controller' do
-    expect(get: '/v1/appointments/1').to route_to(
+    expect(get: "/v1/appointments/#{@appointment.id}").to route_to(
       controller: 'v1/appointments',
       action: 'show',
-      id: '1'
+      id: "#{@appointment.id}"
     )
   end
 
   it 'routes /appointments/:id to the appointments controller' do
-    expect(delete: '/v1/appointments/1').to route_to(
+    expect(delete: "/v1/appointments/#{@appointment.id}").to route_to(
       controller: 'v1/appointments',
       action: 'destroy',
-      id: '1'
+      id: "#{@appointment.id}"
     )
   end
 end
--- spec/routing/investigators_spec.rb
+++ spec/routing/investigators_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'routes for Investigators', type: :routing do
+  before(:each) do
+    @investigator = Investigator.create!(id: 1, name: 'Investi Doe', photo: 'https://photos.com/photo1.jpg', description: 'Once a doe, always a doetective', fee: 45.99, rating: 4)
+  end
+
   it 'routes /investigators to the investigators controller index method' do
     expect(get: '/v1/investigators').to route_to(
       controller: 'v1/investigators',
@@ -16,18 +20,18 @@ RSpec.describe 'routes for Investigators', type: :routing do
   end
 
   it 'routes /investigators/:id to the investigators controller show method' do
-    expect(get: '/v1/investigators/1').to route_to(
+    expect(get: "/v1/investigators/#{@investigator.id}").to route_to(
       controller: 'v1/investigators',
       action: 'show',
-      id: '1'
+      id: "#{@investigator.id}"
     )
   end
 
   it 'routes /investigators/:id to the investigators controller destroy method' do
-    expect(delete: '/v1/investigators/1').to route_to(
+    expect(delete: "/v1/investigators/#{@investigator.id}").to route_to(
       controller: 'v1/investigators',
       action: 'destroy',
-      id: '1'
+      id: "#{@investigator.id}"
     )
   end
 end
