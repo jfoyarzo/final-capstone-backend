ae2330703d6efb33f2e6e02cb446d72486314a3b
MattGomb <gombosonline@gmail.com>
2023-03-17 18:32:16 -0500

    Adjust cors function

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,6 +12,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
              credentials: true,
              headers: :any,
+             expose: ['access-control-allow-origin'],
              methods: %i[get post put patch delete options head]
   end
 end
