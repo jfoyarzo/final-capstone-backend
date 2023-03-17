f024cbba5c27d51f794a90c316ace169a8251e7d
jfoyarzo <foyarzo621@gmail.com>
2023-03-17 18:38:01 -0300

    Add new line to application.rb

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,7 @@ module FinalCapstoneBackend
     config.session_store :cookie_store, key: '_interslice_session'
       config.middleware.use ActionDispatch::Cookies
       config.middleware.use config.session_store, config.session_options
+      config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
   end
   
 end
