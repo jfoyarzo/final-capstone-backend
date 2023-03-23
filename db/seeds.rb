94f9595a544f00e4844b886c72bfe143c1cf409d
MattGomb <gombosonline@gmail.com>
2023-03-23 18:05:20 -0500

    Add placeholder pics that actually work

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,70 +35,70 @@ p "Created #{User.count} User records on the database"
 Investigator.create!([
   {
     name: 'Investi Gator',
-    photo: 'https://example.com/photo1.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2021/02/21/18/05/cat-6037355_1280.png',
     description: 'A true and mighty investigator, with a big mouth and a lot of teeth',
     fee: 69.69,
     rating: 5
   },
   {
     name: 'Investi Cat',
-    photo: 'https://example.com/photo2.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2021/02/21/18/05/cat-6037355_1280.png',
     description: 'Small and cute, but with a big heart and a big appetite. Mainly for fish.',
     fee: 45.66,
     rating: 4
   },
   {
     name: 'Investi Dog',
-    photo: 'https://example.com/photo3.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2021/02/21/18/05/cat-6037355_1280.png',
     description: 'With his tail wagging, he will sniff out the truth for you.',
     fee: 53.98,
     rating: 5
   },
   {
     name: 'Investi Goat',
-    photo: 'https://example.com/photo4.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2021/02/21/18/05/cat-6037355_1280.png',
     description: 'The G.O.A.T. of investigators with a nose for the truth. He will eat anything, but he will not eat your lies.',
     fee: 36.88,
     rating: 5
   },
   {
     name: 'Investi Ghost',
-    photo: 'https://example.com/photo5.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2021/02/21/18/05/cat-6037355_1280.png',
     description: 'A ghostly investigator who will haunt you until you tell the truth.',
     fee: 77.66,
     rating: 4
   },
   {
     name: 'Investi Moth',
-    photo: 'https://example.com/photo6.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2021/02/21/18/05/cat-6037355_1280.png',
     description: 'A moth who will fly into the darkest corners of your mind to find the truth.',
     fee: 22.22,
     rating: 4
   },
   {
     name: 'Investi Raptor',
-    photo: 'https://example.com/photo7.jpg',
+    photo: 'hhttps://cdn.pixabay.com/photo/2021/02/21/18/05/cat-6037355_1280.png',
     description: 'A raptor who will hunt you down until you tell the truth.',
     fee: 38.99,
     rating: 5
   },
   {
     name: 'Investi Grass Owl',
-    photo: 'https://example.com/photo8.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2021/02/21/18/05/cat-6037355_1280.png',
     description: 'A grass owl who will hide in the grass and wait for you to tell the truth. Not super effective.',
     fee: 19.99,
     rating: 3
   },
   {
     name: 'Investi Gecko',
-    photo: 'https://example.com/photo9.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2021/02/21/18/05/cat-6037355_1280.png',
     description: 'A gecko who will climb the walls of your mind to find the truth. He will also eat your flies (not your files)',
     fee: 46.22,
     rating: 5
   },
   {
     name: 'Investi Gazelle',
-    photo: 'https://example.com/photo10.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2021/02/21/18/05/cat-6037355_1280.png',
     description: 'A gazelle who will run circles around you until you tell the truth. She is tireless.',
     fee: 72.19,
     rating: 5
